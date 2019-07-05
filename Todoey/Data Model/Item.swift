84049474f7f9d16d580b83aeee06c5ff3a6ea4fd
Tyquell Brimfield <brimfield17@gmail.com>
2019-07-05 15:22:42 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Tyquell Brimfield on 7/4/19.
+//  Copyright © 2019 Tyquell Brimfield. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+
+var title : String = ""
+var done : Bool = false
+}
